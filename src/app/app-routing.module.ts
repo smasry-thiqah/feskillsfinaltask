867a0d9b4a519d21e7842be63c2b5d5f0f1d3a73
Sayed M. Masry <a.smasry@dev.thiqah.sa>
2023-02-01 03:37:45 +0300

    add netlify build and redicts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,24 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () =>
-      import('./home-page/home-page.module').then((m) => m.HomePageModule),
-  },
-  {
-    path: 'products',
-    loadChildren: () =>
-      import('./products-list-page/products-list-page.module').then(
-        (m) => m.ProductsListPageModule
-      ),
-  },
+  // {
+  //   path: '',
+  //   loadChildren: () =>
+  //     import('./home-page/home-page.module').then((m) => m.HomePageModule),
+  // },
+  // {
+  //   path: 'products',
+  //   loadChildren: () =>
+  //     import('./products-list-page/products-list-page.module').then(
+  //       (m) => m.ProductsListPageModule
+  //     ),
+  // },
   {
     path: 'products/:slug',
     loadChildren: () =>
       import('./product-page/product-page.module').then(
         (m) => m.ProductPageModule
       ),
+  },
+  {
+    path: '**',
+    redirectTo: 'products/item',
   },
 ];
 
